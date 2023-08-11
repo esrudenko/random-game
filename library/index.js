4c5ce7d66ef0dadd909978844cd5c0a62cc85be5
Elena Mankerova <esrudenko93@gmail.com>
2023-08-11 15:47:18 +0600

    fix: did library-part 2

--- library/index.js
+++ library/index.js
@@ -1,5 +1,26 @@
+let hamburger = document.querySelector(".hamburger");
+let navigation = document.querySelector(".navigation");
+const navigationItem = document.querySelectorAll(".navigation__item");
+let navOverlay = document.querySelector(".hamburger-overlay");
 let cardNumber = document.querySelector('.card-number');
 let cardName = document.querySelector('.card-name');
+
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navigation.classList.toggle("active");
+    navOverlay.classList.toggle("active");
+}
+navigationItem.forEach(n => n.addEventListener("click", closeMenu));
+navOverlay.addEventListener("click", closeMenu);
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navigation.classList.remove("active");
+    navOverlay.classList.remove("active");
+}
+
+
+
 
 function inputCard(e) {
     this.value = this.value.replace(/[^\w\d\s-]/g, '')
