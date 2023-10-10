dd0e4d83bd809a5eccc3f21bea2a58d7e4ba3886
Elena Mankerova <esrudenko93@gmail.com>
2023-10-10 15:49:16 +0600

    feat: add js code Move snake

--- random-game/index.js
+++ random-game/index.js
@@ -41,12 +41,12 @@ function makeSnake() {
     let posY = Math.round(Math.random() * (numberOfCells - 1) + 1);
     return [posX, posY];
 }
-let coordinates = makeSnake();
+let snakeCoordinates = makeSnake();
 
 //Create Snake
-let snakeBody = [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'),
-document.querySelector('[posX = "' + (+coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'),
-document.querySelector('[posX = "' + (+coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')];
+let snakeBody = [document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + snakeCoordinates[1] + '"]'),
+document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'),
+document.querySelector('[posX = "' + (+snakeCoordinates[0] - 2) + '"][posY = "' + snakeCoordinates[1] + '"]')];
 for (let i = 0; i < snakeBody.length; i++) {
     snakeBody[i].classList.add('snakeBody');
 }
@@ -75,3 +75,84 @@ header.appendChild(input);
 input.classList.add('input');
 let score = 0;
 input.value = `Score: ${score}`;
+
+//Move Snake
+function moveSnake() {
+    let snakeCoordinates = [snakeBody[0].getAttribute('posX'), snakeBody[0].getAttribute('posY')];
+    snakeBody[0].classList.remove('snakeHead');
+    snakeBody[snakeBody.length - 1].classList.remove('snakeBody');
+    snakeBody.pop();
+    if (direction == 'right') {
+        if (snakeCoordinates[0] < numberOfCells) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    } else if (direction == 'left') {
+        if (snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+snakeCoordinates[0] - 1) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + (+numberOfCells) + '"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    }
+    else if (direction == 'up') {
+        if (snakeCoordinates[1] < numberOfCells) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] + 1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'));
+        }
+    } else if (direction == 'down') {
+        if (snakeCoordinates[1] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+snakeCoordinates[1] - 1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' + snakeCoordinates[0] + '"][posY = "' + (+numberOfCells) + '"]'));
+        }
+    }
+    if (snakeBody[0].getAttribute('posX') == mouse.getAttribute('posX') && snakeBody[0].getAttribute('posY') == mouse.getAttribute('posY')) {
+        mouse.classList.remove('mouse');
+        let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
+        snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        createMouse();
+        score++;
+        input.value = `Score: ${score}`;
+    }
+    if (snakeBody[0].classList.contains('snakeBody')) {
+        setTimeout(() => {
+            gameOver.classList.add('active');
+            let inputOver = document.createElement('input');
+            gameOver.appendChild(inputOver);
+            inputOver.classList.add('score-over');
+            inputOver.value = `Your score: ${score}`;
+            localStorage.setItem(`CurrentScore`, `${score}`);
+            overwriteScoreTable();
+        }, 200)
+        isPaused = true;
+    }
+    snakeBody[0].classList.add('snakeHead');
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeBody[i].classList.add('snakeBody');
+    }
+    steps = true;
+}
+window.addEventListener('keydown', function (event) {
+    if (steps = true) {
+        if (event.keyCode == 37 && direction != 'right') {
+            direction = 'left';
+            steps = false;
+        }
+        if (event.keyCode == 38 && direction != 'down') {
+            direction = 'up';
+            steps = false;
+        }
+        if (event.keyCode == 39 && direction != 'left') {
+            direction = 'right';
+            steps = false;
+        }
+        if (event.keyCode == 40 && direction != 'up') {
+            direction = 'down';
+            steps = false;
+        }
+    }
+})
+
